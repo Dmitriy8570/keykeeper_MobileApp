e4981dbdb8f6b85721cb6f39e81e78cd0cecbf91
Dmitriy8570 <bardakov.dmitriy09@gmail.com>
2025-06-22 19:47:06 +0700

    Initial Commit

---
+++ keykeeper/keykeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  keykeeperApp.swift
+//  keykeeper
+//
+//  Created by Мой Самый лучший любимый on 22.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct keykeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
